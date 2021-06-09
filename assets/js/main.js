4844776247971d40b11621fa4db1c9958b335e49
Hafidzuddin Kautsar Fahmi <hafidzuddink@gmail.com>
2021-06-09 11:23:05 +0700

    done responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,24 @@ window.onscroll = function () {
     }
   });
 };
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
